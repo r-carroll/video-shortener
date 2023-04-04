e73e33b10e79d9b4ce5c20150dba98f6713ed554
ryan.carroll <ryan.carroll@covermymeds.com>
2023-04-04 15:59:06 -0400

    before adding timestamps

--- LSAv2.py
+++ LSAv2.py
@@ -48,30 +48,31 @@ def upload_to_bucket(bucket_name, file_name):
     return blob.public_url
 
 def get_transcription():
-    # client = speech.SpeechClient()
-    # speech_audio = speech.RecognitionAudio(uri='gs://sermon-speech-audio/temp_audio.wav')
-    # config = speech.RecognitionConfig(
-    #     encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-    #     sample_rate_hertz=44100,
-    #     language_code='en-US',
-    #     audio_channel_count = 2,
-    #     enable_automatic_punctuation=True,)
-    # operation = client.long_running_recognize(config=config, audio=speech_audio)
-    # print("Waiting for operation to complete...")
-    # response = operation.result(timeout=5400)
+    client = speech.SpeechClient()
+    speech_audio = speech.RecognitionAudio(uri='gs://sermon-speech-audio/temp_audio.wav')
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=44100,
+        language_code='en-US',
+        audio_channel_count = 2,
+        enable_automatic_punctuation=True,
+        enable_word_time_offsets=True)
+    operation = client.long_running_recognize(config=config, audio=speech_audio)
+    print("Waiting for operation to complete...")
+    response = operation.result(timeout=5400)
 
-    # transcript = ""
-    # for result in response.results:
-    #     transcript += result.alternatives[0].transcript.strip() + " "
+    transcript = ""
+    for result in response.results:
+        transcript += result.alternatives[0].transcript.strip() + " "
 
-    # transcript_file = open("transcript.txt", "w")
-    transcript_file = open("transcript.txt", "r")
-    text = transcript_file.read()
-    # transcript_file.write(transcript)
+    transcript_file = open("transcript.txt", "w")
+    # transcript_file = open("transcript.txt", "r")
+    # text = transcript_file.read()
+    transcript_file.write(transcript)
     transcript_file.close()
 
-    return text
-    # return transcript
+    # return text
+    return transcript
 
 def compute_lsa_scores(sentences):
     vectorizer = CountVectorizer(stop_words='english')
@@ -89,7 +90,7 @@ def compute_sentiment_scores(sentences):
         sentiment_scores.append(sentiment)
     return sentiment_scores
 
-def extract_important_segments():
+def extract_important_segments_old():
     #upload_to_bucket(bucket_name, audio_path)
     transcript = get_transcription()
     sentences = re.split('[.!?]', transcript)
@@ -105,11 +106,29 @@ def extract_important_segments():
     for i in range(len(cluster_centers)):
         start_index = cluster_indices[i, 0]
         end_index = start_index + next((i for i in range(start_index, len(sentences) - 1) if len(sentences[i]) > 1 and len(sentences[i+1]) > 1), len(sentences) - 1) - start_index
-        start_time = sum(len(sentences[j]) + 1 for j in range(start_index))
-        end_time = sum(len(sentences[j]) + 1 for j in range(end_index + 1))
-        important_segments.append({'start_time': start_time, 'end_time': end_time})
+    start_time = sum(len(sentences[j]) + 1 for j in range(start_index))
+    end_time = sum(len(sentences[j]) + 1 for j in range(end_index + 1))
+    important_segments.append({'start_time': start_time, 'end_time': end_time})
     return important_segments
 
+def extract_important_segments():
+    transcript = get_transcription()
+    sentences = re.split('[.!?]', transcript)
+    lsa_scores = compute_lsa_scores(sentences)
+    sentiment_scores = compute_sentiment_scores(sentences)
+    features = np.column_stack((lsa_scores, sentiment_scores))
+    kmeans = KMeans(n_clusters=int(original_video.duration/segment_duration), random_state=42)
+    kmeans.fit(features)
+    cluster_centers = kmeans.cluster_centers_
+    cluster_indices = np.argsort(np.linalg.norm(features - cluster_centers[:, np.newaxis], axis=2), axis=1)
+    important_segments = []
+    for i in range(len(cluster_centers)):
+        start_index = cluster_indices[i, 0]
+        end_index = cluster_indices[i, -1]
+        start_time = sum(len(sentences[j]) + 1 for j in range(start_index))
+        end_time = sum(len(sentences[j]) + 1 for j in range(start_index, end_index+1))
+        important_segments.append({'start_time': start_time, 'end_time': end_time})
+    return important_segments
 
 
 def condense_segment(segment, duration):
