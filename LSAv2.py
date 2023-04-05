1ccbf0f476910272bb24f1251abe2b7947a70b17
ryan.carroll <ryan.carroll@covermymeds.com>
2023-04-05 18:20:52 -0400

    execution makes it through the video file

--- LSAv2.py
+++ LSAv2.py
@@ -1,6 +1,7 @@
 import os
 import io
 import re
+import json
 import math
 import numpy as np
 import moviepy.editor as mp
@@ -48,6 +49,7 @@ def upload_to_bucket(bucket_name, file_name):
     return blob.public_url
 
 def get_transcription():
+    transcript_file = 'sentences.json'
     client = speech.SpeechClient()
     speech_audio = speech.RecognitionAudio(uri='gs://sermon-speech-audio/temp_audio.wav')
     config = speech.RecognitionConfig(
@@ -57,63 +59,51 @@ def get_transcription():
         audio_channel_count = 2,
         enable_automatic_punctuation=True,
         enable_word_time_offsets=True)
-    operation = client.long_running_recognize(config=config, audio=speech_audio)
-    print("Waiting for operation to complete...")
-    response = operation.result(timeout=5400)
+    
+    if os.path.exists(transcript_file):
+        with open(transcript_file, "r") as f:
+            sentences = json.load(f)
+    else:
+        operation = client.long_running_recognize(config=config, audio=speech_audio)
+        print("Waiting for operation to complete...")
+        response = operation.result(timeout=5400)
+        sentences = []
+        for result in response.results:
+            alternatives = result.alternatives
+            
+            for alternative in alternatives:
+                sentence = alternative.transcript.strip()
+                if sentence:
+                    sentences.append({
+                        'text': sentence,
+                        'start_time': alternative.words[0].start_time.total_seconds(),
+                        'end_time': alternative.words[-1].end_time.total_seconds()
+                    })
+        with open('sentences.json', 'w') as f:
+            json.dump(sentences, f)
 
-    transcript = ""
-    for result in response.results:
-        transcript += result.alternatives[0].transcript.strip() + " "
-
-    transcript_file = open("transcript.txt", "w")
-    # transcript_file = open("transcript.txt", "r")
-    # text = transcript_file.read()
-    transcript_file.write(transcript)
-    transcript_file.close()
-
-    # return text
-    return transcript
+    return sentences
 
 def compute_lsa_scores(sentences):
+    texts = [sentence['text'] for sentence in sentences]
     vectorizer = CountVectorizer(stop_words='english')
-    X = vectorizer.fit_transform(sentences).astype(float)
+    X = vectorizer.fit_transform(texts).astype(float)
     lsa = TruncatedSVD(n_components=1, algorithm='arpack', random_state=42)
     lsa_scores = np.abs(lsa.fit_transform(X))
     return lsa_scores
 
 def compute_sentiment_scores(sentences):
+    texts = [sentence['text'] for sentence in sentences]
     client = language_v1.LanguageServiceClient()
     sentiment_scores = []
-    for sentence in sentences:
-        document = language_v1.Document(content=sentence, type_=language_v1.Document.Type.PLAIN_TEXT, language='en')
+    for text in texts:
+        document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT, language='en')
         sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment.score
         sentiment_scores.append(sentiment)
     return sentiment_scores
 
-def extract_important_segments_old():
-    #upload_to_bucket(bucket_name, audio_path)
-    transcript = get_transcription()
-    sentences = re.split('[.!?]', transcript)
-    lsa_scores = compute_lsa_scores(sentences)
-    sentiment_scores = compute_sentiment_scores(sentences)
-    features = np.column_stack((lsa_scores, sentiment_scores))
-
-    kmeans = KMeans(n_clusters=int(original_video.duration/segment_duration), random_state=42)
-    kmeans.fit(features)
-    cluster_centers = kmeans.cluster_centers_
-    cluster_indices = np.argsort(np.linalg.norm(features - cluster_centers[:, np.newaxis], axis=2), axis=1)
-    important_segments = []
-    for i in range(len(cluster_centers)):
-        start_index = cluster_indices[i, 0]
-        end_index = start_index + next((i for i in range(start_index, len(sentences) - 1) if len(sentences[i]) > 1 and len(sentences[i+1]) > 1), len(sentences) - 1) - start_index
-    start_time = sum(len(sentences[j]) + 1 for j in range(start_index))
-    end_time = sum(len(sentences[j]) + 1 for j in range(end_index + 1))
-    important_segments.append({'start_time': start_time, 'end_time': end_time})
-    return important_segments
-
 def extract_important_segments():
-    transcript = get_transcription()
-    sentences = re.split('[.!?]', transcript)
+    sentences = get_transcription()
     lsa_scores = compute_lsa_scores(sentences)
     sentiment_scores = compute_sentiment_scores(sentences)
     features = np.column_stack((lsa_scores, sentiment_scores))
@@ -125,8 +115,8 @@ def extract_important_segments():
     for i in range(len(cluster_centers)):
         start_index = cluster_indices[i, 0]
         end_index = cluster_indices[i, -1]
-        start_time = sum(len(sentences[j]) + 1 for j in range(start_index))
-        end_time = sum(len(sentences[j]) + 1 for j in range(start_index, end_index+1))
+        start_time = sentences[start_index]['start_time']
+        end_time = sentences[end_index]['end_time']
         important_segments.append({'start_time': start_time, 'end_time': end_time})
     return important_segments
 
@@ -135,8 +125,20 @@ def condense_segment(segment, duration):
     start_time = segment['start_time']
     end_time = segment['end_time']
     video_segment = original_video.subclip(start_time, end_time)
-    condensed_segment = video_segment.fx(volumex, 0.5).set_duration(duration)
-    return condensed_segment
+    # condensed_segment = video_segment.set_duration(duration)
+    return video_segment
+
+def concatenate_segments(segments, duration):
+    clips = []
+    for segment in segments:
+        condensed_segment = condense_segment(segment, duration)
+        if condensed_segment.duration <= 0:
+            continue
+        clips.append(condensed_segment)
+    if not clips:
+        return None
+    final_clip = concatenate_videoclips(clips, method='compose')
+    return final_clip
 
 video = mp.VideoFileClip(video_file_name)
 audio = video.audio.write_audiofile(audio_path)
@@ -149,8 +151,9 @@ audio = video.audio.write_audiofile(audio_path)
 
 important_segments = extract_important_segments()
 condensed_segments = []
-for segment in important_segments:
-    condensed_segment = condense_segment(segment, segment_duration)
-    condensed_segments.append(condensed_segment)
-summary_video = concatenate_videoclips(condensed_segments)
-summary_video.write_videofile('summary_video.mp4')
+# for segment in important_segments:
+#     condensed_segment = condense_segment(segment, segment_duration)
+#     condensed_segments.append(condensed_segment)
+# pdb.set_trace()
+summary_video = concatenate_segments(important_segments, 60)
+summary_video.write_videofile('summary_video.mp4', fps=24, codec='libx264', audio_codec='aac')
