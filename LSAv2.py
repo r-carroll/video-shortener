ea7fe52f0deb32c75ad493f18453087a99475250
ryan.carroll <ryan.carroll@covermymeds.com>
2023-04-08 09:00:31 -0400

    trip segments

--- LSAv2.py
+++ LSAv2.py
@@ -31,6 +31,7 @@ bucket_name = 'sermon-speech-audio'
 video_file_name = 'By-Faith.mp4'
 original_video = VideoFileClip(video_file_name)
 segment_duration = 60
+TARGET_DURATION = 20 * 60;
 
 def upload_to_bucket(bucket_name, file_name):
     storage_client = storage.Client()
@@ -121,17 +122,16 @@ def extract_important_segments():
     return important_segments
 
 
-def condense_segment(segment, duration):
+def condense_segment(segment):
     start_time = segment['start_time']
     end_time = segment['end_time']
     video_segment = original_video.subclip(start_time, end_time)
-    # condensed_segment = video_segment.set_duration(duration)
     return video_segment
 
-def concatenate_segments(segments, duration):
+def concatenate_segments(segments):
     clips = []
     for segment in segments:
-        condensed_segment = condense_segment(segment, duration)
+        condensed_segment = condense_segment(segment)
         if condensed_segment.duration <= 0:
             continue
         clips.append(condensed_segment)
@@ -140,20 +140,97 @@ def concatenate_segments(segments, duration):
     final_clip = concatenate_videoclips(clips, method='compose')
     return final_clip
 
-video = mp.VideoFileClip(video_file_name)
-audio = video.audio.write_audiofile(audio_path)
 
-# with io.open(audio_path, 'rb') as audio_file:
-#     content = audio_file.read()
+def create_summary_video(segments, target_duration):
+    summary_segments = []
+    total_duration = 0
+    index = 0
+
+    while index < len(segments) and total_duration < target_duration:
+        segment = segments[index]
+        if total_duration + calculate_segment_duration(segment) <= target_duration:
+            # Add the segment to the summary video
+            summary_segments.append(segment)
+            total_duration += calculate_segment_duration(segment)
+        else:
+            remaining_duration = target_duration - total_duration
+            trimmed_segment = trim_segment(segment, remaining_duration)
+            summary_segments.append(trimmed_segment)
+            total_duration += calculate_segment_duration(trimmed_segment)
+
+        index += 1
+
+    sorted_segments = sorted(segments, key=lambda s: s["start_time"])
+    summary_video = concatenate_segments(sorted_segments)
+
+    return summary_video
+
+def calculate_segment_duration(segment):
+    return segment["end_time"] - segment["start_time"]
+
+def get_longest_subsegment(segment, max_duration):
+    if calculate_segment_duration(segment) <= max_duration:
+        return segment
+    else:
+        # Find the longest subsegment that fits in the remaining time
+        subsegment = segment.copy()
+        subsegment["end_time"] = segment["start_time"] + max_duration
+        return subsegment
+    
+def trim_segment(segment, max_duration):
+    start_time = segment["start_time"]
+    end_time = segment["end_time"]
+    duration = calculate_segment_duration(segment)
+
+    if duration <= max_duration:
+        return segment
+
+    # Calculate the proportion of the segment to keep
+    proportion = max_duration / duration
+
+    # Calculate the new end time
+    new_end_time = start_time + (proportion * (end_time - start_time))
+
+    # Create a new segment with the trimmed time range
+    trimmed_segment = {
+        "start_time": start_time,
+        "end_time": new_end_time,
+    }
+
+    return trimmed_segment
+
+def trim_segments(segments):
+    # Create a dictionary to map start times to segments
+    segment_dict = {segment["start_time"]: segment for segment in segments}
+
+    # Sort the start times in ascending order
+    start_times = sorted(segment_dict.keys())
+
+    # Iterate over the start times and trim each segment
+    trimmed_segments = []
+    previous_end_time = 0
+    for start_time in start_times:
+        segment = segment_dict[start_time]
+
+        # If the segment overlaps with the previous one, trim it
+        if segment["start_time"] < previous_end_time:
+            segment["start_time"] = previous_end_time
+
+        # Add the trimmed segment to the list
+        trimmed_segments.append(segment)
+        previous_end_time = segment["end_time"]
+
+    # Restore the original order of the segments
+    sorted_segments = sorted(trimmed_segments, key=lambda s: segments.index(s))
+
+    return sorted_segments
+
 
 # #upload_to_bucket(bucket_name, audio_path)
-# summarize_video(input_path)
 
 important_segments = extract_important_segments()
-condensed_segments = []
-# for segment in important_segments:
-#     condensed_segment = condense_segment(segment, segment_duration)
-#     condensed_segments.append(condensed_segment)
-# pdb.set_trace()
-summary_video = concatenate_segments(important_segments, 60)
+condensed_segments = [] # might remove this in a bit
+trimmed_segments = trim_segments(important_segments)
+summary_video = create_summary_video(trimmed_segments, TARGET_DURATION)
+# summary_video = concatenate_segments(important_segments, 60)
 summary_video.write_videofile('summary_video.mp4', fps=24, codec='libx264', audio_codec='aac')
